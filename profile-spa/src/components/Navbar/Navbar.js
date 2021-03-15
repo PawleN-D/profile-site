7373010631c00beb9c20ea5470c70a30914eaf8c
PawleN-D <sirpawle@gmail.com>
2021-03-15 17:31:30 +0200

    added cra into the project, created navbar component, removed extra cra files

---
+++ profile-spa/src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Nav, NavbarContainer } from "./Navbar.elements";
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>We Here!!</NavbarContainer>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
